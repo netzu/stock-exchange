304744ff38ffd13fa564060f43e3ada82a52d2c6
Daria Turobos <daria.turobos@gmail.com>
2014-06-03 16:39:26 +0200

    improving test coverage for DataFileReader

--- stock-exchange-commons/src/test/java/data/DataFileReaderTest.java
+++ stock-exchange-commons/src/test/java/data/DataFileReaderTest.java
@@ -24,38 +24,26 @@ public class DataFileReaderTest {
 	@Test
 	public void checkingIfGetStockTickerCollection() throws Exception {
 		DataFileReader dataFileReader = new DataFileReader();
-		URL resource = this.getClass().getClassLoader()
-				.getResource("data/DataFileReaderTest_Inputfile.mst");
+		URL resource = this.getClass().getClassLoader().getResource("data/DataFileReaderTest_Inputfile.mst");
 		File inputFile = new File(resource.getPath());
 
-		StockTickerHistory currentResults = dataFileReader
-				.getStockTickerCollection(inputFile);
+		StockTickerHistory currentResults = dataFileReader.getStockTickerCollection(inputFile);
 		StockTicker ticker = currentResults.getStockTickerDataList().get(0);
 
-		assertTrue("Size of the StockTicker is diffrent that expected(1), has "
-				+ currentResults.getStockTickerDataList().size(),
-				currentResults.getStockTickerDataList().size() == 1);
-		assertTrue("StockTicker name than expected", ticker.getStockName()
-				.equals("Test"));
-		assertTrue("StockTicker Date diffrent than expected", ticker.getDate()
-				.equals(dateFormater.parseDateTime("20120315")));
-		assertTrue("StockTicker Open Value diffrent than expected",
-				ticker.getOpen() == 15.51);
-		assertTrue("StockTicker Highest diffrent than expected",
-				ticker.getHigh() == 7.9);
-		assertTrue("StockTicker Lowest diffrent than expected",
-				ticker.getLow() == 7.54);
-		assertTrue("StockTicker Closed diffrent than expected",
-				ticker.getClose() == 7.54);
-		assertTrue("StockTicker Volumen diffrent than expected",
-				ticker.getVolumen() == 502);
+		assertTrue("Size of the StockTicker is diffrent that expected(1), has " + currentResults.getStockTickerDataList().size(), currentResults.getStockTickerDataList().size() == 1);
+		assertTrue("StockTicker name than expected", ticker.getStockName().equals("Test"));
+		assertTrue("StockTicker Date diffrent than expected", ticker.getDate().equals(dateFormater.parseDateTime("20120315")));
+		assertTrue("StockTicker Open Value diffrent than expected", ticker.getOpen() == 15.51);
+		assertTrue("StockTicker Highest diffrent than expected", ticker.getHigh() == 7.9);
+		assertTrue("StockTicker Lowest diffrent than expected", ticker.getLow() == 7.54);
+		assertTrue("StockTicker Closed diffrent than expected", ticker.getClose() == 7.54);
+		assertTrue("StockTicker Volumen diffrent than expected", ticker.getVolumen() == 502);
 	}
 
 	@Test
 	public void emptyFileWithoutHeader() throws ParseException {
 		DataFileReader dataFileReader = new DataFileReader();
-		URL resource = this.getClass().getClassLoader()
-				.getResource("data/emptyFile_withoutHeader");
+		URL resource = this.getClass().getClassLoader().getResource("data/emptyFile_withoutHeader");
 		File filesWithHeader = new File(resource.getPath());
 
 		String expectedErrorMessage = "File with socks data is empty";
@@ -64,44 +52,34 @@ public class DataFileReaderTest {
 			dataFileReader.getStockTickerCollection(filesWithHeader);
 			fail("Exception not found when expected");
 		} catch (StockDataReaderExcetion ex) {
-			assertTrue(
-					"Error message diffrent than expected: " + ex.getMessage(),
-					ex.getMessage().equals(expectedErrorMessage));
+			assertTrue("Error message diffrent than expected: " + ex.getMessage(), ex.getMessage().equals(expectedErrorMessage));
 		}
 	}
 
 	@Test
 	public void emptyFileWithHeader() throws ParseException {
 		DataFileReader dataFileReader = new DataFileReader();
-		URL resource = this.getClass().getClassLoader()
-				.getResource("data/emptyFile_withHeader");
+		URL resource = this.getClass().getClassLoader().getResource("data/emptyFile_withHeader");
 		File filesWithHeader = new File(resource.getPath());
 
 		String expectedErrorMessage = "File with socks data is empty";
 
 		try {
-			StockTickerHistory tickerHistory = dataFileReader
-					.getStockTickerCollection(filesWithHeader);
-			assertTrue("StockTickerHistory is not empty as expected",
-					tickerHistory.getStockTickerDataList().isEmpty());
+			StockTickerHistory tickerHistory = dataFileReader.getStockTickerCollection(filesWithHeader);
+			assertTrue("StockTickerHistory is not empty as expected", tickerHistory.getStockTickerDataList().isEmpty());
 		} catch (StockDataReaderExcetion ex) {
-			assertTrue(
-					"Error message diffrent than expected: " + ex.getMessage(),
-					ex.getMessage().equals(expectedErrorMessage));
+			assertTrue("Error message diffrent than expected: " + ex.getMessage(), ex.getMessage().equals(expectedErrorMessage));
 		}
 	}
 
 	@Test
 	public void fileWithMultipleEntries() throws ParseException {
 		DataFileReader reader = new DataFileReader();
-		URL resource = this.getClass().getClassLoader()
-				.getResource("data/fileWithMultipleEntries");
+		URL resource = this.getClass().getClassLoader().getResource("data/fileWithMultipleEntries");
 		File inputFile = new File(resource.getPath());
 
-		StockTickerHistory expectedResults = mock
-				.readTickerData("data/fileWithMultipleEntries");
-		StockTickerHistory currentResults = reader
-				.getStockTickerCollection(inputFile);
+		StockTickerHistory expectedResults = mock.readTickerData("data/fileWithMultipleEntries");
+		StockTickerHistory currentResults = reader.getStockTickerCollection(inputFile);
 
 		assertTrue(currentResults.equals(expectedResults));
 	}
@@ -112,6 +90,17 @@ public class DataFileReaderTest {
 		File inputFile = new File("/data/fakeFile");
 
 		reader.getStockTickerCollection(inputFile);
+	}
+	
+	@Test
+	public void fileWithoutHeader() throws ParseException{
+		DataFileReader dataFileReader = new DataFileReader();
+		URL resource = this.getClass().getClassLoader().getResource("data/fileWithoutHeader");
+		File filesWithoutHeader = new File(resource.getPath());
 
+		StockTickerHistory expectedResults = mock.readTickerData("data/fileWithoutHeader");
+		StockTickerHistory currentResults = dataFileReader.getStockTickerCollection(filesWithoutHeader);
+
+		assertTrue(currentResults.equals(expectedResults));
 	}
 }
